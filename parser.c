af5782d98e1b520e51c9aa145d0bc08bd756f496
Nicholas Tidwell <nicktidwell52@gmail.com>
2020-09-01 13:04:42 -0400

    debug issues

--- parser.c
+++ parser.c
@@ -19,20 +19,38 @@ tokenlist* new_tokenlist(void);
 void add_token(tokenlist* tokens, char* item);
 void free_tokens(tokenlist* tokens);
 char* checkCommand(char*);
-void executeCmd(char*, char*, char*, char*);
+void executeCmd(char*, char*, char**, int);
 
 
-//char* builtin_str[] = {
-//  "cd",
-//  "echo"
-//};
-//int (*builtin_func[]) (char**) = {
-//  &f_cd,
-//  &f_echo,
-//  &lsh_exit
-//};
+char* builtin_str[] = {
+  "cd",
+  "echo",
+  "exit"
+};
+int (*builtin_func[]) (char**) = {
+	//&f_cd,
+	//&f_echo,
+	//&f_exit
+};
+int builin_size = 3;
+
+int f_echo(char** argv) {
+	printf("This should echo the command");
+}
+
+int f_cd(char** argv) {
+
+}
+
+int f_exit(char** argv) {
+
+}
+
+
 int main()
 {
+
+
 	while (1) {
 		printf("%s@%s : %s > ", getenv("USER"), getenv("MACHINE"), getenv("PWD")); //This is the prompt
 
@@ -42,7 +60,6 @@ int main()
 
 		char* input = get_input();
 		char* cmdPath = "";
-		char* cmdArg = "";
 		char* fOutput = "";
 		char* fInput = "";
 		int builtIn = 0;
@@ -58,12 +75,15 @@ int main()
 			//This is the first arguement which is the command
 			if (i == 0) {
 
-				if (strcmp(token, "echo") == 0){
+				if (strcmp(token, "echo") == 0) {
 					cmdPath = "echo";
 					builtIn = 1;
 				}
 				else {
 					cmdPath = checkCommand(token);
+					if (cmdPath == NULL) {
+						break;
+					}
 					//Free Space, assign value, move memory space forward
 					argv[argI] = (char*)malloc(strlen(cmdPath));
 					strcpy(argv[argI], cmdPath);
@@ -72,42 +92,41 @@ int main()
 			}
 
 			if (token[0] == '-') {
-
-				if (strcmp(cmdArg, "") == 0) {
-					cmdArg = (char*)malloc(strlen(token) + 1);
-					strcpy(cmdArg, token);
-				}
-				else {
-					memmove(token, token + 1, strlen(token));
-					char * res = (char*)malloc(strlen(cmdArg) + strlen(token) + 1);
-					strcpy(res, cmdArg);
-					strcat(res, token);
-
-					cmdArg = (char*)malloc(strlen(cmdArg) + strlen(token) + 1);
-					strcpy(cmdArg, res);
-				}
+				//Free Space, assign value, move memory space forward
+				argv[argI] = (char*)malloc(strlen(token));
+				strcpy(argv[argI], token);
+				argI++;
 			}
 
-		
+
 			//This Check is To see if token is environment variable
 			if (token[0] == '$') {
 				memmove(token, token + 1, strlen(token)); //Moves pointer forward trimming off $
-				token = (char*)realloc(token, strlen(getenv(token)) + 1); //Allocate New Space for token
-				strcpy(token, getenv(token)); //Assign token to enveronment variable
+				const char* cpy = (char*)malloc(strlen(token) + 1);
+				strcpy(cpy, token);
+				token = (char*)malloc(strlen(getenv(cpy)) + 1); //Allocate New Space for token
+				strcpy(token, getenv(cpy)); //Assign token to enveronment variable
+
+				argv[argI] = (char*)malloc(strlen(token));
+				strcpy(argv[argI], token);
+				argI++;
 			}
 
 			if (token[0] == '~') {
-
 				//Replaces ~ with home environment path
 				memmove(token, token + 1, strlen(token));
 				char* cpy = (char*)malloc(strlen(token));
 				strcpy(cpy, token);
 				token = (char*)realloc(token, (strlen(token) + strlen(getenv("HOME")) + 2));
-				sprintf(token, "%s%s", getenv("HOME"), cpy);				
+				sprintf(token, "%s%s", getenv("HOME"), cpy);
+
+				argv[argI] = (char*)malloc(strlen(token));
+				strcpy(argv[argI], token);
+				argI++;
 			}
 
 			if (token[0] == '>') {
-		
+
 				fOutput = tokens->items[++i];
 			}
 
@@ -115,12 +134,18 @@ int main()
 
 				fInput = tokens->items[++i];
 			}
-			//printf("token %d: (%s)\n", i, token);
 		}
-		
+
 
 		if (builtIn == 0) {
-			executeCmd(cmdPath, cmdArg, fOutput, fInput);
+			executeCmd(fOutput, fInput, argv, argI);
+		}
+		else {
+			for (int i = 0; i < builin_size; i++) {
+				if (strcmp(argv[0], builtin_str[i]) == 0) {
+					return (*builtin_func[i])(argv);
+				}
+			}
 		}
 		free(input);
 		free_tokens(tokens);
@@ -129,40 +154,40 @@ int main()
 	return 0;
 }
 
-tokenlist *new_tokenlist(void)
+tokenlist* new_tokenlist(void)
 {
-	tokenlist *tokens = (tokenlist *) malloc(sizeof(tokenlist));
+	tokenlist* tokens = (tokenlist*)malloc(sizeof(tokenlist));
 	tokens->size = 0;
 	tokens->items = NULL;
 	return tokens;
 }
 
-void add_token(tokenlist *tokens, char *item)
+void add_token(tokenlist* tokens, char* item)
 {
 	int i = tokens->size;
 
-	tokens->items = (char **) realloc(tokens->items, (i + 1) * sizeof(char *));
-	tokens->items[i] = (char *) malloc(strlen(item) + 1);
+	tokens->items = (char**)realloc(tokens->items, (i + 1) * sizeof(char*));
+	tokens->items[i] = (char*)malloc(strlen(item) + 1);
 	strcpy(tokens->items[i], item);
 
 	tokens->size += 1;
 }
 
-char *get_input(void)
+char* get_input(void)
 {
-	char *buffer = NULL;
+	char* buffer = NULL;
 	int bufsize = 0;
 
 	char line[5];
 	while (fgets(line, 5, stdin) != NULL) {
 		int addby = 0;
-		char *newln = strchr(line, '\n');
+		char* newln = strchr(line, '\n');
 		if (newln != NULL)
 			addby = newln - line;
 		else
 			addby = 5 - 1;
 
-		buffer = (char *) realloc(buffer, bufsize + addby);
+		buffer = (char*)realloc(buffer, bufsize + addby);
 		memcpy(&buffer[bufsize], line, addby);
 		bufsize += addby;
 
@@ -170,20 +195,20 @@ char *get_input(void)
 			break;
 	}
 
-	buffer = (char *) realloc(buffer, bufsize + 1);
+	buffer = (char*)realloc(buffer, bufsize + 1);
 	buffer[bufsize] = 0;
 
 	return buffer;
 }
 
-tokenlist *get_tokens(char *input)
+tokenlist* get_tokens(char* input)
 {
-	char *buf = (char *) malloc(strlen(input) + 1);
+	char* buf = (char*)malloc(strlen(input) + 1);
 	strcpy(buf, input);
 
-	tokenlist *tokens = new_tokenlist();
+	tokenlist* tokens = new_tokenlist();
 
-	char *tok = strtok(buf, " ");
+	char* tok = strtok(buf, " ");
 	while (tok != NULL) {
 		add_token(tokens, tok);
 		tok = strtok(NULL, " ");
@@ -193,7 +218,7 @@ tokenlist *get_tokens(char *input)
 	return tokens;
 }
 
-void free_tokens(tokenlist *tokens)
+void free_tokens(tokenlist* tokens)
 {
 	for (int i = 0; i < tokens->size; i++)
 		free(tokens->items[i]);
@@ -201,7 +226,7 @@ void free_tokens(tokenlist *tokens)
 	free(tokens);
 }
 
-char *checkCommand(char* token) {
+char* checkCommand(char* token) {
 	int cmdExist = 0;
 	const char* delimeter = ":";
 	char* pathToken; //Get first path token
@@ -227,62 +252,42 @@ char *checkCommand(char* token) {
 	}
 
 	printf("Command not found\n");
-	return "";
-	
-	
+	return NULL;
+
+
 }
 
-void executeCmd(char* cmdPath, char* cmdArgs, char* output, char* input) {
-	if (strcmp(cmdPath, "") != 0) {
+void executeCmd(char* output, char* input, char** argv, int argI) {
+	int pid = fork();
 
-		int pid = fork();
+	if (pid == 0) {
+		/*This is a child proccess*/
 
-		if (pid == 0) {
-			/*This is a child proccess*/
-
-			char** argv = malloc(5 * sizeof(char*)); //Create Arguement list that allows 5 entries
-			int argI = 0; //Holds index of next free memory space
-
-			//Free Space, assign value, move memory space forward
-			argv[argI] = (char*)malloc(strlen(cmdPath));
-			strcpy(argv[argI], cmdPath);
-			argI++;
-
-			if (strcmp(output, "") != 0) {
-				//Redirects Childs proccess standard output to file
-				int output_fd = open(output, O_WRONLY | O_CREAT | O_TRUNC);
-				dup2(output_fd, STDOUT_FILENO);
-				dup2(output_fd, STDERR_FILENO);
-				close(output_fd);
-			}
-
-			if (strcmp(input, "") != 0) {
-				int fd0 = open(input, O_RDONLY, 0);
-				dup2(fd0, STDIN_FILENO);
-				close(fd0);
-
-				argv[argI] = (char*)malloc(strlen(input));
-				strcpy(argv[argI], input);
-				argI++;
-			}
-
-
-			if (strcmp(cmdArgs, "") != 0) {
-				argv[argI] = (char*)malloc(strlen(cmdArgs));
-				strcpy(argv[argI], cmdArgs);
-				argI++;
-			}
-
-			argv[argI] = NULL;
-			int result = execv(argv[0], argv);
-	
-
-			exit(0);
-			
+		if (strcmp(output, "") != 0) {
+			//Redirects Childs proccess standard output to file
+			int output_fd = open(output, O_WRONLY | O_CREAT | O_TRUNC);
+			dup2(output_fd, STDOUT_FILENO);
+			dup2(output_fd, STDERR_FILENO);
+			close(output_fd);
 		}
-		else {
-			pid = wait(NULL); //Wait for child process to finish
+
+		if (strcmp(input, "") != 0) {
+			int fd0 = open(input, O_RDONLY, 0);
+			dup2(fd0, STDIN_FILENO);
+			close(fd0);
+
 		}
+
+		argv[argI] = NULL;
+		int result = execv(argv[0], argv);
+
+
+		exit(0);
+
 	}
+	else {
+		pid = wait(NULL); //Wait for child process to finish
+	}
+
 
 }
